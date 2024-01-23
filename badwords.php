0139c07b07e3788f163c6839911fe321eb89d8df
igorgaidolfi <gandalf.4@outlook.it>
2024-01-23 17:09:58 +0100

    created form and php logic ES Completato

--- badwords.php
+++ badwords.php
@@ -1,11 +1,24 @@
+<?php  
+    $paragraph = $_GET["paragraph"];
+    $badword = $_GET["badword"];
+    $new_paragraph = str_ireplace($badword, '***',$paragraph);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Badwords</title>
     </head>
     <body>
-        
+        <div>
+            <p>Paragrafo inserito:</p>
+            <?php echo $paragraph;?>
+            <p>Lunghezza paragrafo: <?php echo strlen($paragraph);?> </p>
+            <p>Paragrafo censurato:</p>
+            <?php echo $new_paragraph;?>
+            <p>Lunghezza paragrafo: <?php echo strlen($new_paragraph);?> </p>
+        </div>
     </body>
 </html>
